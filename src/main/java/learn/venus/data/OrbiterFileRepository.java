c7f4aaf63355a35450602bd78378fe88d0f554a7
Sheena <aries.sheen@gmail.com>
2024-03-15 10:05:43 -0700

    Added a new Data Directory and Orbiters CSV file in it

---
+++ src/main/java/learn/venus/data/OrbiterFileRepository.java
@@ -0,0 +1,10 @@
+package learn.venus.data;
+
+public class OrbiterFileRepository {
+
+
+    private final String filePath;
+    public OrbiterFileRepository(String filePath) {
+        this.filePath = filePath;
+    }
+}
