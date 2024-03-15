86cda621f5af05ab408a2f2c90f7eee3a07359e7
Sheena <aries.sheen@gmail.com>
2024-03-15 10:24:32 -0700

    Create the read all method for repository

--- src/main/java/learn/venus/data/OrbiterFileRepository.java
+++ src/main/java/learn/venus/data/OrbiterFileRepository.java
@@ -1,10 +1,16 @@
 package learn.venus.data;
 
+import learn.venus.models.Orbiter;
+import java.util.List;
+
 public class OrbiterFileRepository {
 
 
     private final String filePath;
     public OrbiterFileRepository(String filePath) {
         this.filePath = filePath;
+    }
+    public List<Orbiter> findAll(){
+        return null;
     }
 }
